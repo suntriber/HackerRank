ffaafaee478d9d020730ebb8baf5188d4ed588b1
Patric <suntriber@hotmail.com>
2022-04-26 06:19:22 +0200

    No Idea

---
+++ Python training/No Idea.py
@@ -0,0 +1,11 @@
+"""
+https://www.hackerrank.com/challenges/no-idea/problem?isFullScreen=true
+"""
+
+n, m = input().split()
+
+sc_ar = input().split()
+
+A = set(input().split())
+B = set(input().split())
+print(sum([(i in A) - (i in B) for i in sc_ar]))
