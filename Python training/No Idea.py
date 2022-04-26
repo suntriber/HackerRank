37a65a7bfc5eb4100fc98e3c540c8001e69e25a0
Patric <suntriber@hotmail.com>
2022-04-26 20:10:13 +0200

    verify

--- Python training/No Idea.py
+++ Python training/No Idea.py
@@ -2,10 +2,22 @@
 https://www.hackerrank.com/challenges/no-idea/problem?isFullScreen=true
 """
 
-n, m = input().split()
+# n, m = input().split()
 
-sc_ar = input().split()
 
-A = set(input().split())
-B = set(input().split())
-print(sum([(i in A) - (i in B) for i in sc_ar]))
+# arr = input().split()
+arr = "1 5 7"
+
+# A = set(input().split())
+# B = set(input().split())
+A = {'1', '3', '7'}
+B = {'2', '4', '5'}
+
+
+print(sum([(i in A) - (i in B) for i in arr]))
+print([(i in A) - (i in B) for i in arr])
+
+for i in arr:
+    print(i in A)
+    print(i in B)
+    print(((i in A) - (i in B)))
